15c02d5877699c155dc99db7ce0e24f9a5f27ce9
Simon Martin <smartin@scottlogic.com>
2020-03-10 08:08:08 +0000

    Add logger, expected dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, io};
+use chrono::Local;
+
+#[macro_use]
+extern crate log;
+
+struct ConsoleLogger;
+
+impl log::Log for ConsoleLogger {
+    fn enabled(&self, _metadata: &log::Metadata) -> bool {
+        true
+    }
+
+    fn log(&self, record: &log::Record) {
+        println!("{} {} {} - {}",
+            Local::now().format("%Y-%m-%dT%H:%M:%S%z"),
+            record.level(),
+            record.target(),
+            record.args()
+        );
+     }
+
+    fn flush(&self) {}
+}
+
+static LOGGER: ConsoleLogger = ConsoleLogger;
+
+fn main() -> io::Result<()> {
+    log::set_logger(&LOGGER).unwrap();
+    log::set_max_level(log::LevelFilter::Debug);
+
+    debug!("Hi");
+    trace!("Hello, world!");
+    info!("bye");
+    Ok(())
 }
